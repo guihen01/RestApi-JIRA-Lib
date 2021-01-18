72aef59c7cab9f8d4221964e9b01531be4564766
henri guillot <henri.guihen01@gmail.com>
2021-01-17 21:20:16 -0500

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -12,59 +12,6 @@ namespace JiraLib
 {
 
     /// <summary>
-    ///  execute a POST http request on a Jira server with Rest API 
-    ///  </summary>
-    public class Post
-    {
-
-        /// <summary>
-        ///  execute a POST http request on a Jira server with Rest API, execute json file 
-        ///  <list type="bullet">
-        ///  <item><description><para><em>username : type string : username of a jira account:</em></para></description></item>
-        /// <item><description><para><em>password : type string : password of a jira account:</em></para></description></item>
-        /// <item><description><para><em>pathurl :type string : Jira URL endpoint, on which executing the http post request</em></para></description></item>
-        /// <item><description><para><em>pathjson :type string : json pathname and file containing the POST request to execute </em></para></description></item>
-        /// </list>
-        /// </summary> 
-        public static async System.Threading.Tasks.Task Post1(string username, string password, string pathurl, string pathjson, string result)
-        {
-
-            //REf : https://docs.atlassian.com/software/jira/docs/api/REST/8.13.2/"
-
-            //pathname complet du fichier json
-            //par exemple : C:/C#Rest-API/Curl/Test4-Post/test.json
-            StreamReader sr = new StreamReader(pathjson);
-
-            string json1;
-            json1 = sr.ReadToEnd();
-
-            var json = JsonConvert.SerializeObject(json1);
-            var data = new StringContent(json1, Encoding.UTF8, "application/json");
-
-
-            //var url = "http://localhost:8080/rest/api/2/issue";
-
-            var client = new HttpClient();
-
-
-            var base64String = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
-
-            var response = await client.PostAsync(pathurl, data);
-
-            // It would be better to make sure this request actually made it through
-
-            result = await response.Content.ReadAsStringAsync();
-
-            //close out the client
-            client.Dispose();
-
-            Console.WriteLine(result);
-        }
-    }
-
-
-    /// <summary>
     ///  execute a GET http request on a Jira server with Rest API 
     ///  GEt results  
     ///  </summary>
@@ -157,7 +104,112 @@ namespace JiraLib
             Console.WriteLine("text formated file : List-groups.txt created ");
             Console.WriteLine("----------------------------------------------------------");
         }
+
+
+        /// <summary>
+        ///  execute a GET http request on a Jira server with Rest API, to get all members of a given group. 
+        ///  GEt results as  json file and Text file
+        ///  </summary>
+        public static async System.Threading.Tasks.Task GetUSersFromGroup()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Execute (Jira Server platform) REST API");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine(" REf : https://docs.atlassian.com/software/jira/docs/api/REST/8.13.2/");
+            Console.WriteLine("----------------------------------------------------------------------------");
+
+            string url, url1;
+            Console.WriteLine(" pathname complet du serveur Jira (URL) with port number ? ");
+            Console.WriteLine("as : http://localhost:8080");
+            Console.WriteLine("----------------------------------------------------------------------------");
+            url1 = Console.ReadLine();
+
+            string group;
+            Console.WriteLine("name of the group for which members will be returned ?");
+            group = Console.ReadLine();
+
+
+            url = url1 + "/rest/api/2/group/member?groupname=" + group;
+
+            Console.WriteLine(" URIs for Jira's REST API choosed to pick group's members is : {0} ", url);
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            var client = new HttpClient();
+
+            string user;
+            Console.WriteLine("user account in Jira for authentication");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(" Jira username  ? ");
+            user = Console.ReadLine();
+
+            string password;
+            Console.WriteLine(" Jira password  ? ");
+            password = Console.ReadLine();
+
+
+            var base64String = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{user}:{password}"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
+
+            var response = await client.GetAsync(url);
+            Console.WriteLine(response.StatusCode);
+
+            // It would be better to make sure this request actually made it through
+
+            string result = await response.Content.ReadAsStringAsync();
+
+            //close out the client
+            client.Dispose();
+
+            //wrtite to Console sous forme groupée
+            //---------------------------------------------------------------------------
+            Console.WriteLine(result);
+            Console.WriteLine("----------------------------------------------------------");
+
+            //wrtite to Console sous forme d'objet
+            //---------------------------------------------------------------------------
+            JObject o = JObject.Parse(result);
+            Console.WriteLine(o.ToString());
+            Console.WriteLine("----------------------------------------------------------");
+
+
+            string dir = Directory.GetCurrentDirectory();
+            string path = dir + "/List-users-from-group-" + group + ".json";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(result.ToString());
+                tw.Close();
+            }
+            Console.WriteLine("json formated file : List-users-from-group-{0}.json created ", group);
+            Console.WriteLine("------------------------------------------------------------");
+
+            //write the result sous forme d'objet in a file 
+            // write the result in a text formated file
+            //----------------------------------------------------------------------------
+            //ecriture dans un fichier des données au format string
+
+            string path1 = dir + "/List-users-from-group-" + group + ".txt";
+            if (File.Exists(path1))
+            {
+                File.Delete(path1);
+            }
+            using (var tw1 = new StreamWriter(path1, true))
+            {
+                tw1.WriteLine(o.ToString());
+                tw1.Close();
+            }
+            Console.WriteLine("text formated file : List-users-from-group-{0}.txt created ", group);
+            Console.WriteLine("----------------------------------------------------------");
+
+        }
+
+        
     }
+
+
 
     public class Tools
     {
---
+++ Post.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace JiraLib
+{
+
+    /// <summary>
+    ///  execute a POST http request on a Jira server with Rest API 
+    ///  </summary>
+    public class Post
+    {
+
+        /// <summary>
+        ///  execute a POST http request on a Jira server with Rest API, execute json file 
+        ///  <list type="bullet">
+        ///  <item><description><para><em>username : type string : username of a jira account:</em></para></description></item>
+        /// <item><description><para><em>password : type string : password of a jira account:</em></para></description></item>
+        /// <item><description><para><em>pathurl :type string : Jira URL endpoint, on which executing the http post request</em></para></description></item>
+        /// <item><description><para><em>pathjson :type string : json pathname and file containing the POST request to execute </em></para></description></item>
+        /// </list>
+        /// </summary> 
+        public static async System.Threading.Tasks.Task Post1(string username, string password, string pathurl, string pathjson, string result)
+        {
+
+            //REf : https://docs.atlassian.com/software/jira/docs/api/REST/8.13.2/"
+
+            //pathname complet du fichier json
+            //par exemple : C:/C#Rest-API/Curl/Test4-Post/test.json
+            StreamReader sr = new StreamReader(pathjson);
+
+            string json1;
+            json1 = sr.ReadToEnd();
+
+            var json = JsonConvert.SerializeObject(json1);
+            var data = new StringContent(json1, Encoding.UTF8, "application/json");
+
+
+            //var url = "http://localhost:8080/rest/api/2/issue";
+
+            var client = new HttpClient();
+
+
+            var base64String = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64String);
+
+            var response = await client.PostAsync(pathurl, data);
+
+            // It would be better to make sure this request actually made it through
+
+            result = await response.Content.ReadAsStringAsync();
+
+            //close out the client
+            client.Dispose();
+
+            Console.WriteLine(result);
+        }
+    }
+}
