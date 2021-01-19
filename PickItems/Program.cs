a3727137743df13ec4eb63c645fcf2bb5beb97a3
henri guillot <henri.guihen01@gmail.com>
2021-01-19 17:28:49 -0500

    Add files via upload

---
+++ PickItems/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using JiraLib;
+
+namespace Test5_Pick_Jira_Items
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+           //Returns a list of JIRA, users, groups, name, emailadress matching a specific string
+
+           await JiraLib.Get.PickItems();     
+
+        }
+    }
+}
